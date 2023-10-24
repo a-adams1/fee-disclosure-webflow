6ddb1767d919362e4f9e4814715bbf462256c2a0
a-adams1 <146247628+a-adams1@users.noreply.github.com>
2023-10-24 18:50:58 -0400

    Create webflow.js

---
+++ webflow.js
@@ -0,0 +1,40 @@
+function showFileName() {
+        document.getElementById('infoBox').style.display = 'none';
+        document.getElementById('result').innerHTML = '';
+        var fileInput = document.getElementById('file');
+        var fileNameDiv = document.getElementById('file-name');
+        if (fileInput.files.length > 0) {
+          var fileName = fileInput.files[0].name;
+          var truncateName = truncateFilename(fileName);
+          fileNameDiv.innerHTML = '<strong>Selected file:</strong> ' + truncateName;
+        } else {
+          fileNameDiv.innerHTML = 'No file selected';
+        }
+      }
+      var dropArea = document.getElementById('drop-area');
+      ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+        dropArea.addEventListener(eventName, preventDefaults, false);
+      });
+      function preventDefaults(e) {
+        e.preventDefault();
+        e.stopPropagation();
+      }
+      ['dragenter', 'dragover'].forEach(eventName => {
+        dropArea.addEventListener(eventName, highlight, false);
+      });
+      ['dragleave', 'drop'].forEach(eventName => {
+        dropArea.addEventListener(eventName, unhighlight, false);
+      });
+      function highlight() {
+        dropArea.style.backgroundColor = '#f9f9f9';
+      }
+      function unhighlight() {
+        dropArea.style.backgroundColor = '';
+      }
+      dropArea.addEventListener('drop', handleDrop, false);
+      function handleDrop(e) {
+        var dt = e.dataTransfer;
+        var files = dt.files;
+        document.getElementById('file').files = files;
+        showFileName();
+      }
